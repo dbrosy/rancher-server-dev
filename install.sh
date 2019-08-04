8b7bb9482263e2c5d7a64e14c16fd939efa4492d
dbrosy <dbrosy@hotmail.com>
2019-08-04 22:11:24 +1000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+clear
+
+echo "Installer: Certbot"
+sudo su -
+wget https://dl.eff.org/certbot-auto
+chmod a+x certbot-auto
+#./certbot-auto
+mv certbot-auto /usr/local/bin
+certbot-auto --noninteractive --os-packages-only
+
+echo "Configure: Certbot"
+# ./certbot-auto certonly
+exit
+echo -n "Enter your rancher password and press [ENTER]: "
+read MYSQL_ROOT_PASSWORD
+echo $MYSQL_ROOT_PASSWORD
+if [[ ! -z $MYSQL_ROOT_PASSWORD ]]; then
+  docker-compose up -d
+else
+  echo "Password not set, aborting docker-compose startup"
+fi 
