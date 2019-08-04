3c23dca3d062de38f33c9344f3e749c98615af5f
dbrosy <dbrosy@hotmail.com>
2019-08-04 23:42:56 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ clear
 cert_path="../rancher-server-dev/letsencrypt/live/rancher.lab"
 mkdir -p $cert_path
 
-openssl req -x509 -out $cert_path/privkey.pem -keyout $cert_path/fullchain.pem \
+openssl req -x509 -out $cert_path/fullchain.pem -keyout $cert_path/privkey.pem \
   -newkey rsa:2048 -nodes -sha256 \
   -subj '/CN=rancher.lab' -extensions EXT -config <( \
    printf "[dn]\nCN=rancher.lab\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:rancher.lab\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
