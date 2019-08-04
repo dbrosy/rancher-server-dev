90c2264be8f84e5e52b253d1016faf03b5a23874
dbrosy <dbrosy@hotmail.com>
2019-08-04 22:45:16 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,11 +13,11 @@ clear
 #echo "Configure: Certbot"
 # ./certbot-auto certonly
 #exit
-
+sudo - u
 mkdir -p /etc/letsencrypt/live/rancher.lab
 openssl req -x509 -out /etc/letsencrypt/live/rancher.lab/privkey.pem -keyout /etc/letsencrypt/live/rancher.lab/fullchain.pem   -newkey rsa:2048 -nodes -sha256   -subj '/CN=rancher-lab' -extensions EXT -config <( \ 
    printf "[dn]\nCN=rancher.lab\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:rancher.lab\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
-
+exit
 
 echo -n "Enter your rancher password and press [ENTER]: "
 read MYSQL_ROOT_PASSWORD
