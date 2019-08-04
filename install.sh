d4a10aeb63edfa7551573dc25471897cd7521bb4
dbrosy <dbrosy@hotmail.com>
2019-08-04 22:31:59 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,6 +14,11 @@ clear
 # ./certbot-auto certonly
 #exit
 
+mkdir -p certs
+openssl req -x509 -out certs/privkey.pem -keyout certs/fullchain.pem   -newkey rsa:2048 -nodes -sha256   -subj '/CN=rancher-lab' -extensions EXT -config <( \ 
+   printf "[dn]\nCN=rancher.lab\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:rancher.lab\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+
+
 echo -n "Enter your rancher password and press [ENTER]: "
 read MYSQL_ROOT_PASSWORD
 echo $MYSQL_ROOT_PASSWORD
